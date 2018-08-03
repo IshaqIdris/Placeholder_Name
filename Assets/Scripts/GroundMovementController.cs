82387683572ec1bee1edb9054501390835edf810
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2018-08-03 16:19:36 +1200

    added basic left right movements #2

--- Assets/Scripts/GroundMovementController.cs
+++ Assets/Scripts/GroundMovementController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundMovementController : MonoBehaviour {
+    private Vector2 input;
 
-	// Use this for initialization
-	void Start () {
+	public Transform camPivot;
+	public Transform cam;
+	float heading = 0;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(Input.GetAxis("Horizontal"),0,0);
+		heading += Input.GetAxis("Mouse X")*Time.deltaTime*180;
+		camPivot.rotation = Quaternion.Euler(0,heading,0);
+
+		input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		input = Vector2.ClampMagnitude(input,1);
+
+		Vector3 camF=cam.forward;
+
+		Vector3 camR=cam.right;
+
+		camF.y = 0;
+		camR.y = 0;
+		camF = camF.normalized;
+		camR = camR.normalized;
+		//transform.position += new Vector3(input.x,0,input.y)*Time.deltaTime*5;
+
+		transform.transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*5;
 	}
 }
