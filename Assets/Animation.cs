d7b283cdb25dc26f59897e39558da80ebbb8ee30
Guest User <Guest@wg401-h-07-07.mddn.vuw.ac.nz>
2018-08-04 17:53:12 +1200

    Added humanoid figure from asset store, was able to attach basic ground movement animations #5

---
+++ Assets/Animation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour {
+
+	public Animator animator;
+
+	float inputH;
+	float inputV;
+	
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputH = Input.GetAxis("Horizontal");
+		inputV = Input.GetAxis("Vertical");
+
+		animator.SetFloat("inputH", inputH);
+		animator.SetFloat("inputV", inputV);
+	}
+}
