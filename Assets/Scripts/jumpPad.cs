7a1fe3988901ccd37880451ad30f5718439b9314
Guest User <Guest@wg401-g-02-06.mddn.vuw.ac.nz>
2018-08-16 12:15:07 +1200

    started jumppad implementation #8

---
+++ Assets/Scripts/jumpPad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpPad : MonoBehaviour {
+
+	
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collided!");
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Collided!");
+            collision.gameObject.GetComponent<CharacterController>().Move(new Vector3(0, 500, 0));
+        }
+
+    }
+}
