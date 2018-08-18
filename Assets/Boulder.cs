ad64650c8e4e78ab66c9c9f95574d5ed3cd5b804
Guest User <Guest@wg401-g-03-06.mddn.vuw.ac.nz>
2018-08-19 11:59:45 +1200

    Added boulder roller #10

---
+++ Assets/Boulder.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour {
+
+    GameObject bullet;
+    float timer;
+    public float shootSpeed = 10;
+    float bigBulletTime;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        bigBulletTime = 0;
+        bullet = Resources.Load("Boulder") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        bigBulletTime += Time.deltaTime;
+
+        if (timer > 3)
+        {
+            // = this.spiral * Quaternion.Euler(0, 10, 0);
+            shoot(transform.forward);
+            //shoot(Quaternion.Euler(0, -45, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(Quaternion.Euler(0, 45, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(Quaternion.Euler(0, 135, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(Quaternion.Euler(0, -135, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(Quaternion.Euler(0, -75, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(Quaternion.Euler(0, 75, 0) * new Vector3(0f, 0f, -1f));
+            //shoot(new Vector3(1f, 0f, 0f));
+            //shoot(new Vector3(-1f, 0f, -0f));
+
+            timer = 0;
+        }
+
+
+
+    }
+
+    private void shoot(Vector3 forward)
+    {
+        GameObject projectileForward = Instantiate(bullet) as GameObject;
+        projectileForward.transform.position = transform.position + forward;
+        Rigidbody rb = projectileForward.GetComponent<Rigidbody>();
+
+
+        projectileForward.transform.localScale += new Vector3(1F, 1F, 1F);
+       
+        rb.velocity = forward * shootSpeed;
+
+        Destroy(projectileForward, 2);
+    }
+}
+
