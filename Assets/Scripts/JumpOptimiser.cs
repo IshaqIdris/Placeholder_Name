b7adc22345be675188272148835d45f7b815c8a0
Guest User <Guest@wg401-g-03-06.mddn.vuw.ac.nz>
2018-08-19 12:25:28 +1200

    Added jump optimiser  #11

---
+++ Assets/Scripts/JumpOptimiser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOptimiser : MonoBehaviour
+{
+
+    CharacterController mover;
+    public float fallMultiplier = 10;
+
+    void Start()
+    {
+        mover = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if(mover.velocity.y < 0){
+            mover.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        }
+    }
+
+}
