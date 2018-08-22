9240d2b45e330b6b27b56e13df84ae55b7c23e70
Guest User <Guest@wg401-h-06-07.mddn.vuw.ac.nz>
2018-08-22 14:29:34 +1200

    Attempted to respawn collectables #21

---
+++ Assets/Scripts/RespawnCollectables.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnCollectables : MonoBehaviour
+{
+
+    float respawnTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        respawnTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(this.gameObject.activeSelf);
+        if(!this.gameObject.activeSelf){
+            print("RESPAWN");
+            respawnTime += Time.deltaTime;
+            if(respawnTime > 5){
+                respawnTime = 0;
+                this.gameObject.SetActive(true);
+            }
+        }
+    }
+}
